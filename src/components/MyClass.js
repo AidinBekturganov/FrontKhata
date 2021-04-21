f6e31f75ce2bb551797fda76e380a6e8957a8237
avai7 <aydarov1582@gmail.com>
2021-04-21 15:55:19 +0600

    Button Added to MyClass

--- src/components/MyClass.js
+++ src/components/MyClass.js
@@ -3,10 +3,16 @@ import React, {Component} from 'react';
 
 class MyClass extends Component{
 
+     ClickMe() {
+        alert("BUtton fucked");
+    }
     render(){
         
         return(
+            <div>
             <h1>Nice cock {this.props.email}</h1>
+            <button className = "btn btn-success" onClick={this.ClickMe}> Click me </button>
+            </div>
         )
     }
 }
